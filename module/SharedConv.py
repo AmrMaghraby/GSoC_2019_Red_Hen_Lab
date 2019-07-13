fb42542cf1ce8123f50696dd8afa63ef1df7a18c
AmrMaghraby <amr.alkeleay@gmail.com>
2019-07-12 19:43:09 -0700

    Update SharedConv.py

--- module/SharedConv.py
+++ module/SharedConv.py
@@ -61,9 +61,37 @@ class Backbone(object):
           if i <= 2:
             g[i] = unpool(h[i])
           else:
-            g[i] = slim.conv2(h[i],num_outputs[i],3)
+            g[i] = slim.conv2d(h[i],num_outputs[i],3)
           
-        
-        
-        
-        
+        F_Score = slim.conv2d(g[3],1,1,activation_fn=tf.nn.sigmoid,normalizer_fn=None)
+        geo_map = slim.conv2d(g[3],4,1,activation_fn=tf.nn.sigmoid,normalizer_fn=None) * FLAGS.text_scale
+        angle_map = (slim.conv2d(g[3],1,1,activation_fn = tf.nn.sigmoid,normalizer_fn=None) - 0.5) * np.pi/2
+        F_geometry = tf.concat([geo_map, angle_map],axis=-1)
+    
+    return g[3], F_score, F_geometry  
+  
+  def dice_coefficient(self, y_true_cis, y_pred_cls, training_mask):
+    eps = 1e-5
+    intersection = tf.reduce_sum(y_true_cls * y_pred_cls * training_mask)
+    union = tf.reduce_sum(y_true_cls * training_mask) + tf.reduce_sum(y_pred_cls * training_mask) + eps
+    loss = 1 - (2 * intersection / union)
+    return loss
+  
+  def loss(self, y_true_cls, y_pred_cls, y_true_geo, y_pred_geo, training_mask):
+    classification_loss = self.dice_coefficient(y_true_cls,y_pred_cls,training_mask)
+    classification_loss *= 0.01
+    #d1 -> top d2 -> right d3 -> bottom d4 -> left
+    d1,d2,d3,d4,theta = tf.split(value=y_true_geo, num_or_size_splits=5, axis=3)
+    d1_pred,d2_pred,d3_pred,d4_pred,theta_pred = tf.split(value=y_pred_geo, num_or_size_splits=5, axis=3)
+    area_gt = (d1 + d3) * (d2 + d4)
+    area_pred = (d1_pred + d3_pred) * (d2_pred + d4_pred)
+    w_union = tf.minimum(d2,d2_pred) + tf.minimum(d4,d4_pred)
+    h_union = tf.minimum(d1,d1_pred) + tf.minimum(d3,d3_pred)
+    area_intersect = w_union * h_union
+    area_union = area_gt + area_pred - area_intersect
+    L_AABB = -tf.log((area_intersect + 1.0)/(area_union + 1.0))
+    L_theta = 1 - tf.cos(theta_pred - theta_gt)
+    L_g = L_AABB + 20 * L_theta
+    
+    return tf.reduce_mean(L_g * y_true_cls * training_mask) + classification_loss
+    
